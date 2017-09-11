a8bd33649a79fd109f7d1ea3b32e2c4480a8ffad
Troy Coutu <Autre31415@gmail.com>
2017-09-11 13:21:09 -0400

    initialize repo

---
+++ README.md
@@ -0,0 +1,18 @@
+roosevelt-closure
+===
+
+[UglifyJS](https://github.com/mishoo/UglifyJS2) support for [Roosevelt MVC web framework](https://github.com/rooseveltframework/roosevelt).
+
+# Usage
+
+Declare the `roosevelt-uglify` module as a dependency in the package.json of your roosevelt app.
+
+Then declare your JS compiler by passing it as a param to Roosevelt:
+
+```js
+"rooseveltConfig": {
+  "jsCompiler": {nodeModule: "roosevelt-uglify", params: {someParam: someValue}}
+}
+```
+
+See the [UglifyJS API docs](https://github.com/mishoo/UglifyJS2#api-reference) for documentation on available params.
---
+++ roosevelt-uglify.js
@@ -0,0 +1,37 @@
+const UglifyJS = require('uglify-js').minify
+const fs = require('fs')
+const path = require('path')
+
+module.exports = {
+  parse: function (app, fileName) {
+    let code = fs.readFileSync(path.join(app.get('jsPath'), fileName), 'utf-8')
+    let options = app.get('params').jsCompiler.params || {}
+    let result
+    let newJs
+    let errors
+    let warnings
+
+    // port showWarnings param over to uglify params
+    if (app.get('params').jsCompiler.showWarnings) {
+      options.warnings = true
+    }
+
+    result = UglifyJS(code, options)
+    newJs = result.code
+    errors = result.error
+
+    // only populated when warnings option passed
+    warnings = result.warnings
+
+    if (warnings) {
+      console.warn('⚠️  UglifyJS Warnings:'.bold.yellow)
+      console.warn(warnings)
+    }
+
+    if (errors) {
+      throw errors
+    }
+
+    return newJs
+  }
+}
